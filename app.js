852e7a7411adcf5418b72428c30510560104bdca
AkeraFos <fosterakera20@gmail.com>
2022-11-06 18:38:11 -0500

    Add existing fiel

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
-      fetch('superheroes.php')
-      .then(response => response.text())
-      .then(data => {
-      // Here's some data!
-      alert(data)
-      })
-      .catch(error => {
-      console.log(error);
-      });
+window.addEventListener('load', () => {
+  fetch('superheroes.php')
+  .then(response => response.text())
+  .then(data => {
+  // Here's some data!
+  alert(data)
+  })
+  .catch(error => {
+  console.log(error);
+});
+     
+});
+ 
