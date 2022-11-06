87cd96e8a44e96dbfdca5a1e9ff3ee50ed5aff00
AkeraFos <fosterakera20@gmail.com>
2022-11-06 18:21:10 -0500

    Exercise 2

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-window.addEventListener('load', function() {
-  let btn = document.querySelector('btn');
 
-  btn.addEventListener('click', function(element) {
-      element.preventDefault();
-
-
-  fetch('superheroes.php')
-  .then(response => response.text())
-  .then(data => {
-  alert(data)
-})
-.catch(error => {
-  console.log(error);
-})
+      fetch('superheroes.php')
+      .then(response => response.text())
+      .then(data => {
+      // Here's some data!
+      alert(data)
+      })
+      .catch(error => {
+      console.log(error);
+      });
