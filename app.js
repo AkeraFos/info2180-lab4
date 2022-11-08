c7bbb0dc3a659e20c0577947b0e9ccc514dffc3b
AkeraFos <fosterakera20@gmail.com>
2022-11-07 20:49:21 -0500

    Completed Exercise 2

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+   window.onload = function()  {
+    var sButton = document.getElementById("btn");
+    var httpRequest;
 
-window.addEventListener('load', () => {
-  fetch('superheroes.php')
-  .then(response => response.text())
-  .then(data => {
-  // Here's some data!
-  alert(data)
-  })
-  .catch(error => {
-  console.log(error);
-});
-     
-});
- 
+    sButton.onclick = createRequest;
+
+function createRequest(){
+  event.preventDefault();
+  httpRequest = new XMLHttpRequest();
+  var uInput = document.getElementById("inputField").value;
+  var url = "superheroes.php" + "?query=" + uInput;
+
+  httpRequest.onreadystatechange = recResults;
+  httpRequest.open('GET', url);
+  httpRequest.send();
+}
+
+function recResults() {
+  if(httpRequest.readyState === XMLHttpRequest.DONE) {
+    if(httpRequest.status === 200) {
+      let response = httpRequest.responseText;
+      var resultDiv = document.getElementById("result");
+      resultDiv.innerHTML = response;
+    } else {
+      alert('There was a problem with the request.');
+    }
+  }
+}
+};
